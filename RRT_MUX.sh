66ee7dc611b21f3161538e537363edca690a5ca4
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-06-25 01:24:29 +0330

    Update RRT_MUX.sh

--- RRT_MUX.sh
+++ RRT_MUX.sh
@@ -1,5 +1,87 @@
 #!/bin/bash
 
+# Set the owner and repo variables
+OWNER="radkesvat"
+REPO="WaterWall"
+
+# Determine the architecture and set the ASSET_NAME accordingly
+ARCH=$(uname -m)
+if [ "$ARCH" == "aarch64" ]; then
+  ASSET_NAME="Waterwall-linux-arm64.zip"
+elif [ "$ARCH" == "x86_64" ]; then
+  ASSET_NAME="Waterwall-linux-64.zip"
+else
+  echo "Unsupported architecture: $ARCH"
+  exit 1
+fi
+
+# Function to download and unzip the release
+download_and_unzip() {
+  local url="$1"
+  local dest="$2"
+
+  echo "Downloading $dest from $url..."
+  wget -q -O "$dest" "$url"
+  if [ $? -ne 0 ]; then
+    echo "Error: Unable to download file."
+    return 1
+  fi
+
+  echo "Unzipping $dest..."
+  unzip -o "$dest"
+  if [ $? -ne 0 ]; then
+    echo "Error: Unable to unzip file."
+    return 1
+  fi
+  
+  sleep 0.5
+  chmod +x Waterwall
+  rm "$dest"
+
+  echo "Download and unzip completed successfully."
+}
+
+# Function to get download URL for the latest release
+get_latest_release_url() {
+  local api_url="https://api.github.com/repos/$OWNER/$REPO/releases/latest"
+
+  echo "Fetching latest release data..." >&2
+  local response=$(curl -s "$api_url")
+  if [ $? -ne 0 ]; then
+    echo "Error: Unable to fetch release data." >&2
+    return 1
+  fi
+
+  local asset_url=$(echo "$response" | jq -r ".assets[] | select(.name == \"$ASSET_NAME\") | .browser_download_url")
+  if [ -z "$asset_url" ]; then
+    echo "Error: Asset not found." >&2
+    return 1
+  fi
+
+  echo "$asset_url"
+}
+
+# Function to get download URL for a specific release version
+get_specific_release_url() {
+  local version=$1
+  local api_url="https://api.github.com/repos/$OWNER/$REPO/releases/tags/$version"
+
+  echo "Fetching release data for version $version..." >&2
+  response=$(curl -s $api_url)
+  if [ $? -ne 0 ]; then
+    echo "Error: Unable to fetch release data for version $version." >&2
+    exit 1
+  fi
+
+  local asset_url=$(echo $response | jq -r ".assets[] | select(.name == \"$ASSET_NAME\") | .browser_download_url")
+  if [ -z "$asset_url" ]; then
+    echo "Error: Asset not found for version $version." >&2
+    exit 1
+  fi
+
+  echo $asset_url
+}
+
 setup_waterwall_service() {
     cat > /etc/systemd/system/waterwall.service << EOF
 [Unit]
@@ -32,8 +114,6 @@ while true; do
 
     read -p "Enter your choice: " choice
     if [[ "$choice" -eq 1 || "$choice" -eq 2 ]]; then
-        apt update
-        sleep 0.5
         SSHD_CONFIG_FILE="/etc/ssh/sshd_config"
         CURRENT_PORT=$(grep -E '^(#Port |Port )' "$SSHD_CONFIG_FILE")
 
@@ -46,13 +126,40 @@ while true; do
         sleep 0.5
         mkdir /root/RRT
         cd /root/RRT
-        wget https://github.com/radkesvat/WaterWall/releases/download/v1.18/Waterwall-linux-64.zip
         apt install unzip -y
-        unzip -o Waterwall-linux-64.zip
-        sleep 0.5
-        chmod +x Waterwall
-        sleep 0.5
-        rm Waterwall-linux-64.zip
+        apt install jq -y
+
+        read -p "Do you want to install the latest version? (y/n): " answer
+        if [[ "$answer" == [Yy]* ]]; then
+            # Get the latest release URL
+            url=$(get_latest_release_url)
+
+            if [ $? -ne 0 ] || [ -z "$url" ]; then
+                echo "Failed to retrieve the latest release URL."
+                exit 1
+            fi
+            echo "Latest Release URL: $url"
+            download_and_unzip "$url" "$ASSET_NAME"
+            if [ $? -ne 0 ]; then
+                echo "Failed to download or unzip the file."
+                exit 1
+            fi
+        elif [[ "$answer" == [Nn]* ]]; then
+            read -p "Enter the version you want to install (e.g., v1.18): " version
+            # Get the specific release URL
+            url=$(get_specific_release_url "$version")
+
+            if [ $? -ne 0 ] || [ -z "$url" ]; then
+                echo "Failed to retrieve the latest release URL."
+                exit 1
+            fi
+            echo "Specific Version URL: $url"
+            download_and_unzip "$url" "$ASSET_NAME"
+        else
+            echo "Please answer yes (y) or no (n)."
+            break
+        fi
+
         cat > core.json << EOF
 {
     "log": {
